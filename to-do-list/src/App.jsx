9d4767a37e2934eeda28be5f0aaf2cd9ba8461f5
Disargee18 <aboy.paqs@gmail.com>
2026-02-03 00:30:51 +0800

    ambot galibog ko

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,14 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
+  const [newItem, setNewItem] = useState("");
+  const [newtodo, setNewTodo] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setNewTodo((CurrentTodos) => {
+    return [
+      ...currentTodos,
+      {
+        id: crypto.randomUUID(),
+        title: newItem,
+        completed: false
+      },]});
+
+
+  }
+
   return (
-    <div>Hello World!</div>
+    <>
+      <h1 className='title'>To Do List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Sample Task 1
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Sample Task 2
+          </label>
+        </li>
+      </ul>
+
+      <br /><br />
+      <form className='new-item-form' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="item">New Item</label><br />
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id='item' /> <br /> <br />
+          <button className='add-btn'>Add</button>
+        </div>
+      </form>
+    </>
   )
-    
+
 }
 
 export default App
