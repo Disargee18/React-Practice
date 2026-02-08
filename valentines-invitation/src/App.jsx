8d9c118a4f273c1a8f214551bea9b9c0616c3a92
Disargee18 <aboy.paqs@gmail.com>
2026-02-08 23:06:31 +0800

    finished the first page and structure of it

--- valentines-invitation/src/App.jsx
+++ valentines-invitation/src/App.jsx
@@ -4,24 +4,19 @@ import rizz from './assets/shrek-shrek-rizz.gif'
 import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://tenor.com/embed.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
+  
 
   return (
     <>
-      <div className="bg-[#FADADD] min-h-screen flex justify-center p-20">
+      <div className="bg-[#FADADD] min-h-screen flex justify-center p-15">
         <div className='flex flex-col items-center'>
           <img src={rizz} alt="" className='h-60 m-5' />
-          <p className='text-2xl'>Hey what’s up baby girl, are you free this saturday? *wink</p>
-          <div className='flex flex-row justify-between mt-20'>
-            <div className='bg-green-600 w-13 h-20 w-25 flex items-center justify-center'>
+          <p className='text-2xl m-5'>Hey what’s up baby girl, are you free this saturday? 😉</p>
+          <div className='flex flex-row justify-between mt-10 gap-80'>
+            <div className='bg-green-600 w-13 h-20 w-35 flex items-center justify-center'>
               <h1 className=''>YES</h1>
             </div>
-            <div className='bg-red-600 w-13 h-20 w-25 flex items-center justify-center'>
+            <div className='bg-red-600 w-13 h-20 w-35 flex items-center justify-center'>
               <h1>NO</h1>
             </div>
           </div>
