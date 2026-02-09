32c0cff45e617378af8f49599d18236f5da3f107
Disargee18 <aboy.paqs@gmail.com>
2026-02-10 00:00:03 +0800

    added some functions now

--- valentines-invitation/src/App.jsx
+++ valentines-invitation/src/App.jsx
@@ -1,23 +1,50 @@
 import { use, useState } from 'react'
 import './App.css'
 import rizz from './assets/shrek-shrek-rizz.gif'
+import rizz2 from './assets/rizz-emoji.jpg'
+import rizz3 from './assets/rizz3.jpg'
+import beg from './assets/beg.webp'
 import MainpageContent from './components/MainpageContent.jsx'
 
 function App() {
-  const [show, setShow] = useState(false);
+  const counter = 0;
 
-  const handleYes=()=>{
-    setShow(true);
+  const [content, setContent] = useState({
+    image: rizz,
+    text: "Hey what’s up baby girl, are you free this saturday? 😉"
+  });
+
+  const handleYes = () => {
+    setContent({
+      image: rizz2,
+      text: "Will you be my valentine? 🫦"
+    });
+    counter++;
+  }
+
+  const handleNo = () => {
+    setContent({
+      image: beg,
+      text: "PWEASE SAY YES 😩"
+    });
+  }
+
+  const handleNew = () => {
+    if(counter>2){
+      setContent({
+        image: rizz3,
+        text: "I guess you don't want to be my valentine 😔"
+      });
+    }
   }
 
   return (
     <>
       <div className="bg-[#FADADD] min-h-screen flex justify-center p-15">
         <MainpageContent
-          item={{
-            image: rizz,
-            text: "Hey what’s up baby girl, are you free this saturday? 😉"
-          }}
+          item={content}
+          ifYes={handleYes}
+          ifNo={handleNo}
         />
         
 
--- valentines-invitation/src/components/MainpageContent.jsx
+++ valentines-invitation/src/components/MainpageContent.jsx
@@ -1,14 +1,14 @@
 
-function MainpageContent({item}) {
+function MainpageContent({item, ifYes, ifNo}) {
     return (
         <div className="mainpage-content flex flex-col items-center">
             <img src={item.image} alt="" className='h-60 m-5' />
             <p className='text-2xl m-5'>{item.text}</p>
             <div className='flex flex-row justify-between mt-10 gap-80'>
-                <div className='bg-green-600 w-13 h-20 w-35 flex items-center justify-center'>
+                <div className='bg-green-600 w-13 h-20 w-35 flex items-center justify-center' onClick={ifYes}>
                     <h1 className=''>YES</h1>
                 </div>
-                <div className='bg-red-600 w-13 h-20 w-35 flex items-center justify-center'>
+                <div className='bg-red-600 w-13 h-20 w-35 flex items-center justify-center' onClick={ifNo}>
                     <h1>NO</h1>
                 </div>
             </div>
