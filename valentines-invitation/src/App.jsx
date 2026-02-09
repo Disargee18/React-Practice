b0aa588d227efe89bced2dca913db44e82de7275
Disargee18 <aboy.paqs@gmail.com>
2026-02-09 23:36:03 +0800

    made my intial design into components and currently adding functionalities to it

--- valentines-invitation/src/App.jsx
+++ valentines-invitation/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import rizz from './assets/shrek-shrek-rizz.gif'
-import { useEffect } from 'react'
+import MainpageContent from './components/MainpageContent.jsx'
 
 function App() {
-  
+  const [show, setShow] = useState(false);
+
+  const handleYes=()=>{
+    setShow(true);
+  }
 
   return (
     <>
       <div className="bg-[#FADADD] min-h-screen flex justify-center p-15">
-        <div className='flex flex-col items-center'>
-          <img src={rizz} alt="" className='h-60 m-5' />
-          <p className='text-2xl m-5'>Hey what’s up baby girl, are you free this saturday? 😉</p>
-          <div className='flex flex-row justify-between mt-10 gap-80'>
-            <div className='bg-green-600 w-13 h-20 w-35 flex items-center justify-center'>
-              <h1 className=''>YES</h1>
-            </div>
-            <div className='bg-red-600 w-13 h-20 w-35 flex items-center justify-center'>
-              <h1>NO</h1>
-            </div>
-          </div>
-        </div>
+        <MainpageContent
+          item={{
+            image: rizz,
+            text: "Hey what’s up baby girl, are you free this saturday? 😉"
+          }}
+        />
+        
 
       </div>
     </>
---
+++ valentines-invitation/src/components/MainpageContent.jsx
@@ -0,0 +1,19 @@
+
+function MainpageContent({item}) {
+    return (
+        <div className="mainpage-content flex flex-col items-center">
+            <img src={item.image} alt="" className='h-60 m-5' />
+            <p className='text-2xl m-5'>{item.text}</p>
+            <div className='flex flex-row justify-between mt-10 gap-80'>
+                <div className='bg-green-600 w-13 h-20 w-35 flex items-center justify-center'>
+                    <h1 className=''>YES</h1>
+                </div>
+                <div className='bg-red-600 w-13 h-20 w-35 flex items-center justify-center'>
+                    <h1>NO</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainpageContent;
