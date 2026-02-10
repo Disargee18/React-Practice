08d639bf59fce8282cbab498736c725c0637ccb7
Disargee18 <aboy.paqs@gmail.com>
2026-02-10 18:04:21 +0800

    added some audio for more rizzing effect

--- valentines-invitation/src/App.jsx
+++ valentines-invitation/src/App.jsx
@@ -4,7 +4,11 @@ import rizz from './assets/shrek-shrek-rizz.gif'
 import rizz2 from './assets/rizz-emoji.jpg'
 import rizz3 from './assets/rizz3.jpg'
 import beg from './assets/beg.webp'
+import soundOn from './assets/speaker-filled-audio-tool.png'
+import soundOff from './assets/mute.png'
+import music from './assets/oui.mp3'
 import MainpageContent from './components/MainpageContent.jsx'
+
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -14,8 +18,23 @@ function App() {
     text: "Hey what’s up baby girl, are you free this saturday? 😉"
   });
 
-  const handleYes = () => {
+  const [sound, setSound] = useState(soundOff);
 
+
+  const [audio] = useState(new Audio(music));
+
+  const handleSound = () => {
+    if (sound === soundOff) {
+      setSound(soundOn);
+      audio.play();
+    }else{
+      setSound(soundOff);
+      audio.pause();
+    }
+  }
+
+
+  const handleYes = () => {
     if (counter >= 1) {
       setContent({
         image: rizz3,
@@ -33,7 +52,7 @@ function App() {
   const handleNo = () => {
 
     if (counter >= 1) {
-      setContent( content );
+      setContent(content);
     } else {
       setContent({
         image: beg,
@@ -46,6 +65,11 @@ function App() {
   return (
     <>
       <div className="bg-[#FADADD] min-h-screen flex justify-center p-15">
+        <div>
+          <button onClick={handleSound}>
+            <img src={sound} alt="" className='h-10 absolute top-5 right-5' />
+          </button>
+        </div>
         <MainpageContent
           item={content}
           ifYes={handleYes}
