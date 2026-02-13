06e60c4bb3027e1a4afe30430b727bd828104e7f
Disargee18 <aboy.paqs@gmail.com>
2026-02-13 22:26:04 +0800

    just a short coding for today. fixed the content that will be set in clicking each boxes. will add the checkWinner function tmrw

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -7,83 +7,115 @@ import farquad from './assets/farquad.jpg'
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [box, setBoxes] = useState(Array(9).fill(""));
+  const [turn, setTurn] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
+
+
+  const handleClick = (value) => {
+    console.log(value);
+    if (box[value])
+      return;
+
+    const newBoxes = [...box];
+    if (turn % 2 == 0) {
+      newBoxes[value] = shrek;
+      console.log("shrek");
+    } else {
+      newBoxes[value] = farquad;
+      console.log("farquad");
+
+    }
+    setBoxes(newBoxes);
+    setTurn(turn + 1);
   }
 
-  const [content, setContent] = useState();
-
-  const handleContent = () => {
-    if (count%2 == 0) {
-      setContent(shrek);
-      setCount(count + 1)
-    }else{
-      setContent(farquad);
-      setCount(count + 1)
-    }}
+  
 
 
 
 
 
-    return (
-      <>
-        <div className='w-100 h-full border-1 border-black absolute right-30 top-0 flex flex-col items-center'>
-          <div className='flex flex-col'>
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='text-2xl m-10 '>Shrek vs Mike</h1>
-              <div className='flex flex-row gap-20 items-center justify-center'>
-                <img src={shrek} alt="shrek" className='cursor-pointer size-25' />
-                <img src={farquad} alt="mike" className='cursor-pointer size-25' />
-              </div>
-            </div>
-            <div className='flex flex-col mt-15'>
-              <h1 className='text-2xl my-5'>Points</h1>
-              <div className='align'>
-                <p className='text-lg'>Shrek: {count}</p>
-                <p className='text-lg'>Mike: 0</p>
-              </div>
+  // const handleClick = () => {
+  //   if (count%2 == 0) {
+  //     setContent(shrek);
+  //     setCount(count + 1)
+  //   }else{
+  //     setContent(farquad);
+  //     setCount(count + 1)
+  //   }}
 
+
+
+
+
+  return (
+    <>
+      <div className='w-100 h-full border-1 border-black absolute right-30 top-0 flex flex-col items-center'>
+        <div className='flex flex-col'>
+          <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-2xl m-10 '>Shrek vs Mike</h1>
+            <div className='flex flex-row gap-20 items-center justify-center'>
+              <img src={shrek} alt="shrek" className='cursor-pointer size-25' />
+              <img src={farquad} alt="mike" className='cursor-pointer size-25' />
             </div>
           </div>
+          <div className='flex flex-col mt-15'>
+            <h1 className='text-2xl my-5'>Points</h1>
+            <div className='align'>
+              <p className='text-lg'>Shrek: </p>
+              <p className='text-lg'>Mike: 0</p>
+            </div>
 
-        </div>
-        <div className='flex flex-col items-center gap-4 m-10'>
-          <h1 className='text-2xl'><b>tic-tac-toe</b></h1>
-          <div className='grid grid-cols-3 gap-2'>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-              <img src={content} alt="" />
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-              <img src={content} alt="" />
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-              <img src={content} alt="" />
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
-            <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={handleContent}>
-
-            </div>
           </div>
         </div>
-      </>
-    )
-  }
+
+      </div>
+      <div className='flex flex-col items-center gap-4 m-10'>
+        <h1 className='text-2xl'><b>tic-tac-toe</b></h1>
+        <div className='grid grid-cols-3 gap-2'>
+          {box.map((box, index) => (
+            <div 
+              className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer'
+              onClick={() => handleClick(index)} 
+              key={index}>
+              <img src={box} alt="" />
+            </div>
+          ))}
+
+
+          {/* <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(0)}>
+            <img src={setBoxes} alt="" />
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(1)}>
+            <img src={box} alt="" />
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(2)}>
+            <img src="" alt="" />
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(3)}>
+
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(4)}>
+
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(5)}>
+
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(6)}>
+
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(7)}>
+
+          </div>
+          <div className='w-20 h-20 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer' onClick={() => handleClick(8)}>
+
+          </div> */}
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default App
