760a4eb1eb3197c524d14619c24c00a1af203aa1
Disargee18 <aboy.paqs@gmail.com>
2026-02-19 23:14:12 +0800

    still learning framer motions (learned how to make it into a function, and also how to  add rotation and scaling of objects)

--- framer-practice/src/App.jsx
+++ framer-practice/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { delay, motion } from 'framer-motion'
 
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
     hidden: { opacity: 0 },
     show: {
       opacity: 1,
-        transition: {
-        staggerChildren: 0.50,
-        },
+      transition: {
+        staggerChildren: 0.25,
+      },
     },
   }
 
@@ -19,6 +19,32 @@ function App() {
     hidden: { opacity: 0 },
     show: { opacity: 1 },
   }
+
+  const fadeOut = {
+    hidden: { opacity: 0, y: 100 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        delay: 0.5,
+      }
+    }
+  }
+
+  const fadeIn = {
+    hidden: { opacity: 0, x: -100 },
+    show: { opacity: 1, x: 0, 
+      transition:{
+        duration: 1,
+        ease: "easeIn",
+        delay: 0.75,
+      }
+    }
+  }
+
+
 return (
   <div className='flex flex-col gap-10 overflow-x-hidden'>
     <motion.section variants={sectionVariant}
@@ -26,9 +52,25 @@ return (
       animate='show'
       className='grid grid-cols-3 p-10 gap-10'>
       <motion.div variants={divVariant}
-        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
+        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+        <motion.div className='bg-stone-100 w-20 h-20 rounded-lg'
+          variants={fadeOut}></motion.div>
+        <motion.div className='bg-stone-100 w-20 h-20 rounded-full'
+          variants={fadeIn}></motion.div>
+      </motion.div>
       <motion.div variants={divVariant}
-        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
+        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+
+        <motion.div className='w-1/3 h-1/3 bg-blue-300'
+          animate={{
+            scale: [1, 2, 2, 1],
+            rotate: [0, 90, 180, 360],
+            borderRadius: ["10%", "20%", "30%", "10%"],
+          }}
+          transition={{ duration: 5, ease: "easeOut", repeat: 2, delay: 1 }}
+        ></motion.div>
+
+      </motion.div>
       <motion.div variants={divVariant}
         className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
       <div className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></div>
