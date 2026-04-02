78b613184856bd949c194b48711f39646a157516
Disargee18 <aboy.paqs@gmail.com>
2026-04-02 15:40:36 +0800

    practicing using supabase

---
+++ react-supabase-try/to-do-list/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import supabase from './supabase-client'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Word</h1>
+    </div>
+  )
+}
+
+export default App
